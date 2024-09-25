f53b2bffba3599e33d7f221de6bf072f493c94d4
Owen Rummage <owen@rummage.cc>
2024-09-24 20:40:48 -0500

    fix title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
         <div className="flex flex-col items-center text-center">
-          <h1 className="text-5xl 2xl: font-bold">Our networks power your</h1>
-          <h1 className="text-4xl text-purple-500 font-bold">
+          <h1 className="text-5xl font-bold">Our networks power your</h1>
+          <h1 className="text-4xl 2xl:text-5xl text-purple-500 font-bold">
             <ReactTyped
               strings={[
                 'Silicon-Valley Startup', 
