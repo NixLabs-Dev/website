891a0357ca25d702f479c90144c36dee285f1f73
Owen Rummage <owen@rummage.cc>
2024-09-26 12:48:09 -0500

    fix workflow caching: Fuck, I need to do this on every other service... KILL ME

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,6 +11,7 @@ export default function NavBar() {
                 <div id="Navbar.Container.LinkContainer" className="ml-auto flex flex-row items-center gap-4 text-zinc-400">
                     <a href="/services/carrier" className=" text-md font-medium">Carrier Services</a>
                     <a href="/services/hosting" className="text-md font-medium">Hosting Services</a>
+                    <a href="https://docs.nixlabs.dev" className="text-md font-medium">Developer Documentation</a>
                 </div>
         </div>
     </div>
